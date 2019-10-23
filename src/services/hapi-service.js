d82cf1593e30dd4c3dc54bcfe6769beb37677a61
NickErmak <ermak.bmstu@gmail.com>
2019-10-23 14:37:05 +0300

    'add_service'

---
+++ src/services/hapi-service.js
@@ -0,0 +1,39 @@
+const publicKEY = '$2a$10$SEUUqrFg.ZO75cr2BOh/C.548oHRV8YCPGoY5rPsvXYX1RFYsCn7y';
+
+export default class HapiService {
+
+    theBurrow = 'https://www.potterapi.com/v1';
+
+    async getMagic(url) {
+        const res = await fetch(`${this.theBurrow}${url}`);
+        return await res.json();
+    }
+
+    getAllCharacters() {
+        return this.getMagic(`/characters?key=${publicKEY}`)
+    }
+
+    getCharacter(id) {
+        return this.getMagic(`/characters/${id}?key=${publicKEY}`)
+    }
+
+    getAllSpells() {
+        return this.getMagic(`/spells?key=${publicKEY}`)
+    }
+
+    getAllHouses() {
+        return this.getMagic(`/houses?key=${publicKEY}`)
+    }
+
+    getHouse(id) {
+        return this.getMagic(`/houses/${id}?key=${publicKEY}`)
+    }
+}
+
+// const hapi = new HapiService();
+//
+// hapi.getAllCharacters().then((body) => {
+//     console.log(body);
+// });
+
+
